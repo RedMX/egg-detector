e49b52e430c763a7ff18c0cf610929a7bdbd02b1
RedMX <50285835+RedMX@users.noreply.github.com>
2022-12-12 16:26:09 +0100

    Fixed issues with transparent images
    Fixed issues with transparent images by converting what the AI sees to an array with only 3 color values

--- egg_detector.py
+++ egg_detector.py
@@ -11,11 +11,11 @@ from object_detection.builders import model_builder
 from object_detection.utils import config_util
 from urllib.request import Request, urlopen
 import requests
-imgclnt='' #your imgur ClientID
+imgclnt = '' #your imgur ClientID
 category_index = label_map_util.create_category_index_from_labelmap('./label_map.pbtxt')
-config=config_util.get_configs_from_pipeline_file('./model/pipeline.config')
-model=model_builder.build(model_config=config["model"],is_training=False)
-ckpt=tf.compat.v2.train.Checkpoint(model=model)
+config = config_util.get_configs_from_pipeline_file('./model/pipeline.config')
+model = model_builder.build(model_config=config["model"],is_training=False)
+ckpt = tf.compat.v2.train.Checkpoint(model=model)
 ckpt.restore('./model/ckpt-26').expect_partial()
 
 
@@ -30,8 +30,8 @@ def detect_fn(image):
 
 def img_chck(url):
 
-    score='0'
-    imgurl='0'
+    score ='0'
+    imgurl ='0'
     req = Request(
         url=url,
         headers={'User-Agent': 'Mozilla/5.0'}
@@ -39,8 +39,8 @@ def img_chck(url):
     webpage = urlopen(req).read()
     arr = np.asarray(bytearray(webpage), dtype=np.uint8)
     img = cv2.imdecode(arr, -1)
-
-    image_np = np.array(img)
+    img_dtct = cv2.cvtColor(img,cv2.COLOR_BGRA2BGR)
+    image_np = np.array(img_dtct)
 
     input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
     detections = detect_fn(input_tensor)
@@ -58,17 +58,17 @@ def img_chck(url):
     im_height,im_width,dim=img.shape
     for i in range(len(detections['detection_scores'])):
         if detections['detection_scores'][i]>float(os.environ['egg_ai_threshold']):
-            img = cv2.rectangle(img,(int(detections['detection_boxes'][i][1]*im_width),int(detections['detection_boxes'][i][0]*im_height)) ,(int(detections['detection_boxes'][i][3]*im_width),int(detections['detection_boxes'][i][2]*im_height)), (0,0,255), 5)
+            img = cv2.rectangle(img,(int(detections['detection_boxes'][i][1]*im_width),int(detections['detection_boxes'][i][0]*im_height)) ,(int(detections['detection_boxes'][i][3]*im_width),int(detections['detection_boxes'][i][2]*im_height)), (0,0,255,255), 5)
 
             #img = cv2.putText(img, category_index[detections['detection_classes'][i]+1]['name']+": "+str(np.around(detections['detection_scores'][i]*100,2))+"%",(int(detections['detection_boxes'][i][1]*im_width),int(detections['detection_boxes'][i][0]*im_height)-30),cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),5,cv2.LINE_AA) uncomment for text(may not work well with lower resolution images)
     if np.max(detections['detection_scores']) > float(os.environ['egg_ai_threshold']):
-        #cv2.imwrite('./eggs.jpg',img) write image to file
-        retval, buffer = cv2.imencode('.jpg', img)
+        #cv2.imwrite('./eggs.png',img) #write image to file
+        retval, buffer = cv2.imencode('.png', img)
         img_rsp = requests.request("POST", "https://api.imgur.com/3/image", headers={'Authorization': 'Client-ID '+imgclnt}, data={'image': base64.b64encode(buffer)}, files=[])
         img_rsp = json.loads(img_rsp.content.decode('UTF-8'))
         if img_rsp['success']==True:
-            imgurl=img_rsp['data']['link']
-        score=str(np.around(np.max(detections['detection_scores'])*100,2))
+            imgurl = img_rsp['data']['link']
+        score = str(np.around(np.max(detections['detection_scores'])*100,2))
     return {"score": score, "imgurl": imgurl}
 
 context = zmq.Context()
@@ -77,7 +77,7 @@ socket.bind("tcp://*:5555")
 while True:
  
     message = socket.recv()
-    message=message.decode("utf-8")
+    message = message.decode("utf-8")
     socket.send(str(img_chck(message)).encode(encoding= 'UTF-8'))
 
    
